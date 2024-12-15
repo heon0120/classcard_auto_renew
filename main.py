e712fa54fd803f9c2b4a23f9309a007bc71f8aec
STUDIO CSGNS <76412504+heon0120@users.noreply.github.com>
2024-12-15 09:51:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from selenium.webdriver.common.by import By
 from handler.recall_learning import RecallLearning
 from handler.spelling_learning import SpellingLearning
 from selenium.webdriver.chrome.options import Options
-
+from handler.test_learning import TestLearning
+from handler.rote_learning import RoteLearning
 # 함수불러오기
 from utility import (
     chd_wh,
@@ -35,17 +36,18 @@ driver = webdriver.Chrome(options=chrome_options)
 
 # 로그인 시행
 driver.get("https://www.classcard.net/Login")
-id_element = driver.find_element(By.ID, "login_id")
-pw_element = driver.find_element(By.ID, "login_pwd")
+id_element = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div[1]/input')
+pw_element = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div[1]/div/input')
 id_element.clear() # Autofill 억제
 id_element.send_keys(account["id"])
 pw_element.send_keys(account["pw"])
-time.sleep(1)
+
 driver.find_element(
     By.XPATH,
-    "/html/body/div[1]/div/div/div/div/form/div[3]/a",
+    "/html/body/div[2]/div[2]/div/div/div/form/div[3]/a",
 ).click()  # 로그인 버튼
 time.sleep(1)  # 로딩 대기
+
 
 # 클래스 선택
 class_dict = {}
@@ -53,27 +55,32 @@ class_list_element = driver.find_element(
     By.CSS_SELECTOR,
     "body > div.mw-1080 > div:nth-child(6) > div > div > div.left-menu > div.left-item-group.p-t-none.p-r-lg > div.m-t-sm.left-class-list",
 )
+
+# 클래스 목록 가져오기
 class_count = len(class_list_element.find_elements(By.TAG_NAME, "a"))
 for class_item, i in zip(
-    class_list_element.find_elements(By.TAG_NAME, "a"),
-    range(class_count),
+        class_list_element.find_elements(By.TAG_NAME, "a"),
+        range(class_count),
 ):
     class_temp = {}
     class_temp["class_name"] = class_item.text
     class_temp["class_id"] = class_item.get_attribute("href").split("/")[-1]
+
     if class_temp["class_id"] == "joinClass":
         break
     class_dict[i] = class_temp
 
+# 클래스가 없는 경우 처리
 if len(class_dict) == 0:
     print("클래스가 없습니다.")
     quit()
 elif len(class_dict) == 1:
-    choice_class_val = 0
+    choice_class_val = 0  # 자동으로 첫 번째 클래스 선택
 else:
-    choice_class_val = choice_class(class_dict=class_dict)  # 클래스 입력
+    choice_class_val = choice_class(class_dict=class_dict)  # 클래스 선택
 
-class_id = class_dict[choice_class_val].get("class_id")  # 클래스 아이디 가져오기
+# 선택한 클래스 ID 가져오기
+class_id = class_dict[choice_class_val].get("class_id")
 driver.get(f"https://www.classcard.net/ClassMain/{class_id}")  # 클래스 페이지로 이동
 time.sleep(1)  # 로딩 대기
 
@@ -84,11 +91,13 @@ sets_div = driver.find_element(
 sets = sets_div.find_elements(By.CLASS_NAME, "set-items")
 sets_count = len(sets)
 sets_dict = {}
+
+# 세트 목록 가져오기
 for set_item, i in zip(sets, range(sets_count)):
     set_temp = {}
     set_temp["card_num"] = (
         set_item.find_element(By.TAG_NAME, "a").find_element(By.TAG_NAME, "span").text
-    )  # 카드 개수 가져오기 예) "10 카드"
+    )  # 카드 개수 가져오기
     set_temp["title"] = set_item.find_element(By.TAG_NAME, "a").text.replace(
         set_temp["card_num"], ""
     )  # 카드 개수 제거
@@ -97,12 +106,14 @@ for set_item, i in zip(sets, range(sets_count)):
     )  # 세트 아이디 가져오기
     sets_dict[i] = set_temp
 
-set_choice = choice_set(sets_dict)  # 세트 입력
+# 세트 선택
+set_choice = choice_set(sets_dict)
 set_site = (f"https://www.classcard.net/set/{sets_dict[set_choice]['set_id']}/{class_id}")
 driver.get(set_site)  # 세트 페이지로 이동
 time.sleep(1)  # 로딩 대기
 
-user_id = int(driver.execute_script("return c_u;"))  # API 요청을 위해 유저 아이디 가져오기
+# 유저 ID 가져오기
+user_id = int(driver.execute_script("return c_u;"))
 
 # 단어 저장
 driver.find_element(
@@ -113,17 +124,25 @@ driver.find_element(
     By.CSS_SELECTOR,
     "body > div.test > div.p-b-sm > div.set-body.m-t-25.m-b-lg > div.m-b-md.pos-relative > div.dropdown.open > ul > li:nth-child(1) > a",
 ).click()  # 학습구간 전체로 변경
-html = BeautifulSoup(driver.page_source, "html.parser")  # 페이지 소스를 html로 파싱
+
+# 페이지 소스를 HTML로 파싱
+html = BeautifulSoup(driver.page_source, "html.parser")
 cards_ele = html.find("div", class_="flip-body")  # 카드들을 찾음
 num_d = len(cards_ele.find_all("div", class_="flip-card")) + 1  # 카드의 개수를 구함
 time.sleep(0.5)  # 로딩 대기
-word_d = word_get(driver, num_d)  # 단어를 가져옴
+
+# 단어 가져오기
+word_d = word_get(driver, num_d)
 da_e, da_k, da_kyn = word_d
 
-ch_d = chd_wh()  # 학습유형 입력
-while 1:
+# 학습 유형 선택 및 실행
+while True:
+    ch_d = chd_wh()  # 학습유형 입력
+
     if ch_d == 1:
-        print("암기학습은 지원하지 않습니다.")
+        print("암기학습을 시작합니다.")
+        controler = RoteLearning(driver=driver)  # 암기 학습 클래스 생성
+        controler.run(num_d=num_d)  # 학습 시작
     elif ch_d == 2:
         print("리콜학습을 시작합니다.")
         controler = RecallLearning(driver=driver)  # 암기 학습 클래스 생성
@@ -133,8 +152,9 @@ while 1:
         controler = SpellingLearning(driver=driver)  # 스펠 학습 클래스 생성
         controler.run(num_d=num_d, word_d=word_d)  # 학습 시작
     elif ch_d == 4:
-        print("테스트학습은 지원하지 않습니다.")
-
+        print("스펠학습을 시작합니다.")
+        controler = TestLearning(driver=driver)
+        controler.run(num_d=num_d, word_d=word_d)
     elif ch_d == 5:
         print("암기학습 API 요청을 시작합니다.")
         classcard_api_post(
@@ -162,6 +182,7 @@ while 1:
             view_cnt=num_d,
             activity=3,
         )
+
     print("학습이 종료되었습니다.")
-    driver.get(set_site)  # 다시 세트페이지로 이동
+    driver.get(set_site)  # 다시 세트 페이지로 이동
     time.sleep(1)
